b0d7ddd3d89eb950eda1438bff08f0b6aea6ffa9
Alex <alexandre.merancienne@gmail.com>
2026-05-28 15:40:54 +0300

    Add asynchronous tasks with Celery+Redis

---
+++ backend/shortener/tasks.py
@@ -0,0 +1,23 @@
+from celery import shared_task
+
+from .models import Hit, ShortLink
+from .utils import get_country_code, hash_ip
+
+
+@shared_task
+def create_hit(link_id, ip, referrer):
+    try:
+        link = ShortLink.objects.get(id=link_id)
+    except ShortLink.DoesNotExist:
+        return
+
+    Hit.objects.create(
+        link=link,
+        country=get_country_code(ip),
+        referrer=referrer[:512],
+        ip_hash=hash_ip(ip),
+    )
+
+
+
+    
