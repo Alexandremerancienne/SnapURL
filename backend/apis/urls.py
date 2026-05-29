fb0ca8a8cef34637a32df99c26324ee945d987d5
Alex <alexandre.merancienne@gmail.com>
2026-05-29 10:06:47 +0300

    Check linting

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import AnalyticsViewSet, DashboardStatsView, DashboardUsernameView, LinkViewSet
+from .views import (
+    AnalyticsViewSet,
+    DashboardStatsView,
+    DashboardUsernameView,
+    LinkViewSet,
+)
 
 router = SimpleRouter()
 router.register("links", LinkViewSet, basename="links")
@@ -10,7 +15,9 @@ router.register("analytics", AnalyticsViewSet, basename="analytics")
 
 urlpatterns = router.urls + [
     path("dashboard/stats/", DashboardStatsView.as_view(), name="dashboard-stats"),
-    path("dashboard/links/", LinkViewSet.as_view({"get": "list"}), name="dashboard-links"),  
+    path(
+        "dashboard/links/", LinkViewSet.as_view({"get": "list"}), name="dashboard-links"
+    ),
     path(
         "dashboard/username/",
         DashboardUsernameView.as_view(),
